6304fe8a4f3df0825b1221a00db8da240ba86309
Yuvanna Bawa <76108856+YB73@users.noreply.github.com>
2021-08-29 00:42:05 +0530

    MXEVNSUB

---
+++ LTIME99C/MXEVNSUB.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%4==0 || (n+1)%4==0){
+            cout<<n<<endl;
+        }
+        else{
+            cout<<n-1<<endl;
+        }
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
+   
